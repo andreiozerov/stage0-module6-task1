272393041387a424f2aa24bdf6d9e6695a065f98
Andrei Ozerov <andrei.ozerov@gmail.com>
2022-07-09 21:30:39 +0300

    Task #1. Class creation Done

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,30 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private final String color;
+    private final int numberOfPaws;
+    private final boolean hasFur;
 
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription() {
+        String paw;
+        String hasFurString;
+
+        paw = (numberOfPaws == 1) ? " paw" : " paws";
+        hasFurString = (hasFur) ? " a" : " no";
+
+        return "This animal is mostly " +
+                color +
+                ". It has " +
+                numberOfPaws +
+                paw +
+                " and" +
+                hasFurString +
+                " fur.";
+    }
 }
